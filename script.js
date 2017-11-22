d0ae3d2b6c97f3893ce28fc76ad4f1c4d9fac72a
Oleg Sulyanov <oleg@sulyanov.com>
2017-11-22 06:55:02 +0300

    Add balancing

--- script.js
+++ script.js
@@ -6,31 +6,32 @@ script.src = 'https://code.jquery.com/jquery-2.2.4.min.js';
 document.getElementsByTagName('head')[0].appendChild(script);
 
 var cities = {
-    1: { n: 1051, min: null, max: null },
-    2: { n: 1052, min: null, max: null },
-    3: { n: 1053, min: null, max: null },
-    4: { n: 1054, min: null, max: null },
-    5: { n: 1055, min: null, max: null },
-    6: { n: 1056, min: null, max: null },
-    7: { n: 1057, min: null, max: null },
-    8: { n: 1058, min: null, max: null },
-    9: { n: 1059, min: null, max: null },
-    10: { n: 1060, min: null, max: null },
-    11: { n: 1061, min: null, max: null },
-    12: { n: 1069, min: null, max: null },
-    13: { n: 1213, min: null, max: null },
-    14: { n: 1345, min: null, max: null },
-    15: { n: 1373, min: null, max: null },
-    16: { n: 1384, min: null, max: null },
-    17: { n: 1392, min: null, max: null },
-    18: { n: 1393, min: null, max: null },
-    19: { n: 1394, min: null, max: null },
-    20: { n: 1395, min: null, max: null },
-    21: { n: 1421, min: null, max: null },
-    22: { n: 1438, min: null, max: null }
+    1: {n: 1051, min: null, max: null},
+    2: {n: 1052, min: null, max: null},
+    3: {n: 1053, min: null, max: null},
+    4: {n: 1054, min: null, max: null},
+    5: {n: 1055, min: null, max: null},
+    6: {n: 1056, min: null, max: null},
+    7: {n: 1057, min: null, max: null},
+    8: {n: 1058, min: null, max: null},
+    9: {n: 1059, min: null, max: null},
+    10: {n: 1060, min: null, max: null},
+    11: {n: 1061, min: null, max: null},
+    12: {n: 1069, min: null, max: null},
+    13: {n: 1213, min: null, max: null},
+    14: {n: 1345, min: null, max: null},
+    15: {n: 1373, min: null, max: null},
+    16: {n: 1384, min: null, max: null},
+    17: {n: 1392, min: null, max: null},
+    18: {n: 1393, min: null, max: null},
+    19: {n: 1394, min: null, max: null},
+    20: {n: 1395, min: null, max: null},
+    21: {n: 1421, min: null, max: null},
+    22: {n: 1438, min: null, max: null}
 };
 
 var options = {};
+
 function restore_options() {
     var default_options = {
         interval: 5,
@@ -40,7 +41,7 @@ function restore_options() {
         default_options['g' + i + 'min'] = 10000;
         // default_options['g' + i + 'max'] = 100000;
     }
-    chrome.storage.sync.get(default_options, function(items) {
+    chrome.storage.sync.get(default_options, function (items) {
         options['interval'] = items.interval;
         options['step'] = items.step;
         for (var i = 1; i <= 21; i++) {
@@ -54,33 +55,88 @@ function restore_options() {
         console.log(options)
     });
 }
+
 document.addEventListener('DOMContentLoaded', restore_options);
 restore_options();
 
+$(function () {
+    if($('a:contains("Группа 0. Супергруппа")').is('a')) {
+        checkGroups();
+    }
+});
 
-console.log(cities);
+function checkGroups() {
+    console.log('Start balancing at ' + currentTime());
 
-
-$(function() {
     $.each(cities, function (num, city_options) {
         console.group('Check group ' + num);
         console.log(city_options);
+        if (city_options['min'] == 0) {
+            console.log('Off');
+            console.groupEnd();
+            return true;
+        }
 
         var current_amount = getCityAmount(num);
         console.log(current_amount);
-        if(current_amount <= city_options['min']) {
+        if (current_amount <= city_options['min']) {
             var desired_amount = (city_options['min'] * 100 - current_amount * 100 + options['step'] * 100) / 100;
             console.warn('Fix: add ' + desired_amount);
-
+            addAmount(city_options['n'], desired_amount);
         } else {
             console.log('Ok');
         }
         console.groupEnd();
     });
 
-});
+    $('b:contains("Список групп")').text('Список групп (Последняя балансировка завершена в ' + currentTime() + ')');
+
+    var interval = options['interval'] * 60 * 1000;
+    console.log('Retry in ' + interval + ' milliseconds');
+    setTimeout(function () {
+        location.reload();
+    }, interval);
+}
+
+function addAmount(n, amount) {
+    var url = '?actionName=add_balance_subdealer&sub=' + n;
+    var data = {
+        comment: '',
+        amount_add: amount,
+        type_pay: 'bank',
+        balance_add: ''
+    };
+
+    $.ajax({
+        type: "POST",
+        url: url,
+        cache: false,
+        data: data,
+        complete: function (xhr, status) {
+            console.log('Completed with status: ' + status);
+        }
+    });
+}
 
 function getCityAmount(num) {
     var line = $("table table table tr:contains('Группа " + num + ". ')");
     return parseFloat($("td:nth-child(2)", line).text());
+}
+
+function currentTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    // add a zero in front of numbers<10
+    m = checkTime(m);
+    s = checkTime(s);
+    return h + ":" + m + ":" + s;
+}
+
+function checkTime(i) {
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
 }
